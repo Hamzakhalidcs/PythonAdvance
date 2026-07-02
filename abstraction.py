5bb912bff32a5655613cecafae694458d2a9c6aa
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-07-02 22:17:01 +0500

    added snippet

---
+++ abstraction.py
@@ -0,0 +1,5 @@
+# Abstraction means hiding unnecessary implementation details and showing the only essential features.
+# you know what to do, but you dont need to know how it works internally . 
+
+# python provides a module called abc i.e Abstract Base Class
+# Its is a specaial class that can not be used to create object directly.
--- poly.py
+++ poly.py
@@ -16,6 +16,9 @@ class Cat(Animal):
     def speak(self):
         print("Meowww")
 
+def make_sound(animal):
+    animal.speak()
+
 
 dog = Dog()
 dog.speak()
@@ -24,3 +27,7 @@ cat = Cat()
 cat.speak()
 
 # Notice that the methos is same but behaviour is change depends on the object call.this is polym,orphism
+make_sound(dog)]
+
+# The variable animal references a Dog object. When animal.speak() is executed,
+# Python looks for the speak() method in the Dog class and calls it, which prints "Woof".
