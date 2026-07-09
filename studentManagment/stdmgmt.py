32758140dc0cc0413effe575a4f9c365d1bf0426
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-07-09 18:39:46 +0500

    Update Method

--- studentManagment/stdmgmt.py
+++ studentManagment/stdmgmt.py
@@ -17,7 +17,7 @@ class Book:
         print("Quantity  :", self.quantity)   
 
 
-    def  borrow_book(self):
+    def borrow_book(self):
         print("Book", self.title)
         self.quantity -= 1
 
@@ -27,7 +27,7 @@ class Book:
         self.quantity += 1
 
 
-class Library():
+class Library(Book):
     def __init__(self):
         self.books = []
         for book in self.books:
@@ -37,25 +37,46 @@ class Library():
          self.books.append(book)
 
 
-    def display_books(self):
+    def display_book(self):
+
         for book in self.books:
-            book.display_info()
+            print(book.display_info())
 
+        if not self.books:
+            print("No Books in the Library")
 
+   
     def search_book(self, title):
-        found = False
+        """Return the book object if found, else None."""
 
         search_title = title.lower()
+        
         for book in self.books:
             if book.title.lower() == search_title:
                 book.display_info()
-                found = True
+                return book  # ← Returns the book object
             
-        if not found:
-                print(f"No Book with title {title}")
+        return None  # ← Returns None if not found
+
+    
+    def borrow_book(self, title):
+        """Borrow a book if exist and is available"""
+
+        book = self.search_book(title)
+
+        if not book:
+            print(f" Book {title} not found in Library")
+            return False
         
+        if book.quantity<=0:
+            print(f"'{book.title}' is Currently Unavailable (0 Coopies Left)")
+            return False
         
+        book.borrow_book()
+        print(f"Successfully Borrowed '{book.title}' Book.")
         
+
+
 
 
 python_book = Book(101, "Python", "Eric", 5)
@@ -68,10 +89,13 @@ library.add_book(python_book)
 library.add_book(sql_book)
 library.add_book(data_book)
 
-# library.display_books()
-library.search_book('ISLAMIAT')
+# library.display_book()
+# library.search_book('ISLAMIAT')
+# another_book = python_book
+# print(another_book is python_book)
 
-
+library.borrow_book("Python")
+library.search_book('Python')
 
 
 
