39b71ed720dbea894e59a3c98d5c1328dcf5ab7e
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-08-08 23:34:00 +0500

    Setup Virtual environment and ignore venv

---
+++ VirtualEnvironmentPractice/app.py
@@ -0,0 +1,9 @@
+"""
+activation and installation and checking installation
+source .venv/bin/activate 
+ which pip 
+ pip list 
+ pip install requests
+ pip list string for VirtualEnvironmentPractice.app
+ pip freeze > requirements.txt
+"""
