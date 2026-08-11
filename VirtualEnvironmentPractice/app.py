d090c63854e329942f5ba9d7d3cc6737b911b798
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-08-11 23:03:06 +0500

    Added notes and logging usage in module

--- VirtualEnvironmentPractice/app.py
+++ VirtualEnvironmentPractice/app.py
@@ -7,3 +7,26 @@ source .venv/bin/activate
  pip list string for VirtualEnvironmentPractice.app
  pip freeze > requirements.txt
 """
+
+import logging
+
+# logging.basicConfig(level=logging.INFO)
+
+# logging.debug("Debugging application")
+# logging.info("Application Started")
+# logging.warning("Low disk space")
+# logging.error("Could not connect to database")
+# logging.critical("Application crashed")
+
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    filename= "app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+)
+
+logger.info("Applicaiton Started")
+logger.warning("Student Already Exists")
+logger.error("Unable to save student Data")
