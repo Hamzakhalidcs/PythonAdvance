e69051505e6d1b0cede53e0e44e2300ef4082d58
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-08-12 22:19:06 +0500

    Added logging levels and formatters to use logging

--- VirtualEnvironmentPractice/app.py
+++ VirtualEnvironmentPractice/app.py
@@ -1,32 +1,67 @@
-"""
-activation and installation and checking installation
-source .venv/bin/activate 
- which pip 
- pip list 
- pip install requests
- pip list string for VirtualEnvironmentPractice.app
- pip freeze > requirements.txt
-"""
+# """
+# activation and installation and checking installation
+# source .venv/bin/activate 
+#  which pip 
+#  pip list 
+#  pip install requests
+#  pip list string for VirtualEnvironmentPractice.app
+#  pip freeze > requirements.txt
+# """
+
+# import logging
+
+# # logging.basicConfig(level=logging.INFO)
+
+# # logging.debug("Debugging application")
+# # logging.info("Application Started")
+# # logging.warning("Low disk space")
+# # logging.error("Could not connect to database")
+# # logging.critical("Application crashed")
+
+
+# logger = logging.getLogger(__name__)
+
+# logging.basicConfig(
+#     filename= "app.log",
+#     level=logging.INFO,
+#     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+# )
+
+# logger.info("Applicaiton Started")
+# logger.warning("Student Already Exists")
+# logger.error("Unable to save student Data")
+
+# import database
 
 import logging
 
-# logging.basicConfig(level=logging.INFO)
-
-# logging.debug("Debugging application")
-# logging.info("Application Started")
-# logging.warning("Low disk space")
-# logging.error("Could not connect to database")
-# logging.critical("Application crashed")
-
-
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(
-    filename= "app.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler("app.log")
+stream_handler = logging.StreamHandler()
+
+file_formatter = logging.Formatter(
+    "%(asctime)s, %(levelname)s, %(name)s, %(message)s"
 )
 
-logger.info("Applicaiton Started")
+stream_formatter = logging.Formatter(
+    "%(levelname)s,  %(message)s"
+)
+
+file_handler.setLevel(logging.INFO)
+stream_handler.setLevel(logging.WARNING)
+
+file_handler.setFormatter(file_formatter)
+stream_handler.setFormatter(stream_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
+logger.debug("Debugging application")
+logger.info("Application Started")
 logger.warning("Student Already Exists")
-logger.error("Unable to save student Data")
+logger.error("Unable to save Student")
+logger.critical("Application Crashed")
+
---
+++ VirtualEnvironmentPractice/database.py
@@ -0,0 +1,6 @@
+import logging 
+
+logger = logging.getLogger(__name__)
+
+logger.info("DataBase Module has started")
+logger.error("DataBase Connectino Failed")
