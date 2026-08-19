cb9937edf27c8d6fc937899194bd729e72ef0d07
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-08-19 18:35:36 +0500

    Added logging to the student file and put checks for existing records

--- studentManagment/student.py
+++ studentManagment/student.py
@@ -9,8 +9,24 @@ students = {}
 
 def add_student(student_id, name):
     if student_id in students:
-        print(f"Student with '{student_id}' already exists")
+        # print(f"Student with '{student_id}' already exists")
+        logger.warning(f"Student With ID '{student_id}' already exists! Name :{students[student_id]}")
+
     else:
         students[student_id] = name
+        # print(f"Student '{name}' added Successfully")
+        logger.info(f"Student '{name}' With ID '{student_id}' added Successfully ")
+
+
+add_student(1, "Hamza")
+add_student(1, 'ASAD')
+print(students)
     
 
+success = add_student(2, "John")
+if success:
+    print("Student is added successfully.")
+
+sucess = add_student(2, "Asad")
+if not success:
+    print("Student already exists!")
