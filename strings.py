7b4dbff09090db50ab96d917cadb95ca916d3ebf
Hamzakhalidcs <hamzakhalidcs007@gmail.com>
2026-08-25 21:42:48 +0500

    Added string more methods

--- strings.py
+++ strings.py
@@ -76,3 +76,64 @@ join() changes list -> string
 So  split() -> O(n) and join() -> O(n)
 where n represent the amount of input/output text being processed. 
 """
+print(sentence.find("easy")) #it returns the index where that word start or letter which are trying to find . 
+print(sentence.find("java")) # will return -1 as if not found 
+
+"""
+Current string complexity picture
+
+word[3]        O(1)
+len(word)      O(1)
+word.lower()   O(n)
+word[::-1]     O(n)
+word.split()   O(n)
+" ".join(...)  O(n)
+word.find()    O(n)
+
+Next Useful Operation are 
+count()
+replace()
+strip()
+startswith()
+endswith()
+"""
+
+word = "banana"
+print(word.count('a')) # how much time this character repeated. and time complexity is O(n)
+
+text  = "Python is easy. Python is Powerful"
+
+# Can also count sub string in sentence
+
+print(text.count('Python'))
+
+# Replace values 
+text = "I like java"
+print(text.replace("java", "Python")) #replace does not change the actual text 
+print(text)
+new_text = text.replace("java", "Python")
+print(new_text) #replace complexity is also O(n)
+
+name = "    HAmza   "
+print(name.strip()) #it removes white spaces from start and end, but do not between words. 
+name = "    Hamza Khalid    "
+print(name.strip())
+
+#related methods
+
+print(name.lstrip())    #removes spaces from left 
+print(name.rstrip())    #removes spaces from right
+
+# These are particularly useful when cleaning data coming from files ,userInput, csv etc.
+
+file_name = "report_2026.csv"   #checking the file name or string start with 
+print(file_name.startswith("repo"))
+print(file_name.startswith("sales"))
+
+print(file_name.endswith(".csv"))
+"""
+A very useful for file processing 
+for file in file: 
+    if file.endswith(".csv"):
+       print(file)      apply logic if needed
+"""
